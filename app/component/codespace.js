141c27c88a6748a37690f14a9ebeccb9c27d3c63
SukritiR29 <sukritirajora@gmail.com>
2024-02-14 21:52:04 +0530

    code space

---
+++ app/component/codespace.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Code = () => {
+  return (
+    <div
+      className="w-3/4 border-2 border-slate-900 m-2 bg-gray-900 rounded-xl"
+      style={{ height: "96%" }}
+    ></div>
+  );
+};
+
+export default Code;
