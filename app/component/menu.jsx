66d567658485f573e06a56eb11cd937ea5b5c008
SukritiR29 <sukritirajora@gmail.com>
2024-02-15 22:58:25 +0530

    menu resume

--- app/component/menu.jsx
+++ app/component/menu.jsx
@@ -2,6 +2,7 @@
 
 import React, {useRef} from 'react'
 import { MdArrowDropDown } from "react-icons/md";
+import { MdOutlineOpenInNew } from "react-icons/md";
 
 
 const Menu = () => {
@@ -16,10 +17,17 @@ const Menu = () => {
 
   return (
     <div className='w-1/4 m-2 border-2 border-slate-900 bg-gradient-to-b from-slate-950  to-indigo-950 rounded-xl' style={{height: '96%'}}>
-        <div className='p-4 m-2 rounded-lg bg-gray-800 flex gap-10'>
+        <div className='p-4 m-2 rounded-lg bg-gradient-to-r  from-pink-500 to-orange-500 flex gap-10'>
             <h2 className='text-sm flex flex-row'>Sukriti.dev </h2>
-            <MdArrowDropDown className='flex justify-end ml-20 text-xl text-slate-400' />
+            <MdArrowDropDown className='flex justify-end ml-20 text-xl text-slate-100' />
         </div>
+        <a href="http://resume-sukritir29.vercel.app/">
+        <div className='p-3 m-4 lg:mr-6 rounded-lg border-lime-700 flex gap-16  border gap-10 cursor-pointer hover:bg-gray-900'>
+        <button className='text-xs flex flex-row'>Download Resume</button>
+        <MdOutlineOpenInNew className='justify-end text-xs' />
+
+        </div>
+        </a>
         <div className='p-3 m-4 lg:mr-6 rounded-lg border-slate-700 border gap-10 cursor-pointer hover:bg-gray-900' onClick={() => scrollToSection("about")}>
         <button className='text-xs flex flex-row'>About</button>
         </div>
