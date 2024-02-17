5aea41471b13c0ea1fcfdca28dd3f28af7a03d86
SukritiR29 <sukritirajora@gmail.com>
2024-02-17 08:44:46 +0530

    responsive holder

--- app/component/Holder.jsx
+++ app/component/Holder.jsx
@@ -4,7 +4,7 @@ import Code from './codespace'
 
 const Holder = () => {
   return (
-    <div className='border-2 border-indigo-900 h-4/5 w-5/6 ml-20 rounded-xl flex '>
+    <div className='lg:border-2 lg:border-indigo-900 h-4/5 lg:w-5/6 w-fit lg:ml-20 rounded-xl lg:p-0 p-4 lg:flex lg:flex-row flex flex-col  '>
         <Menu/>
         <Code/>
     </div>
